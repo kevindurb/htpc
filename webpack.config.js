3a9caec8638a8a25374628432f068a57ed900e2e
Kevin Durbin <kevindurb@gmail.com>
2020-08-27 15:09:17 -0600

    add web bundle

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/web.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'web.js'
+  }
+}
